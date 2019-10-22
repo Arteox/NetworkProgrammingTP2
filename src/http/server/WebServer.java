d35be2e9a4bb6398f17e0850486abce96bf3f077
Arteox <ung.louis@hotmail.fr>
2019-10-22 16:38:32 +0200

    ability to send an image

--- src/http/server/WebServer.java
+++ src/http/server/WebServer.java
@@ -2,6 +2,7 @@
 package http.server;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -11,6 +12,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Base64;
 
 public class WebServer {
 
@@ -34,7 +36,7 @@ public class WebServer {
         for (;;) {
             try {
                 Socket remote = s.accept();
-                
+
                 System.out.println("Connection, sending data.");
                 BufferedReader in = new BufferedReader(new InputStreamReader(remote.getInputStream()));
                 PrintWriter out = new PrintWriter(remote.getOutputStream());
@@ -104,6 +106,12 @@ public class WebServer {
             if (postedData.isEmpty()) {
                 out.println("data empty");
             }
+        } else if ("/image".equals(URL)) {
+            File file = new File("doc/image.jpg");
+            byte[] fileContent = Files.readAllBytes(file.toPath());
+            String newImage = "data:image/jpeg;base64,";
+            newImage += Base64.getEncoder().encodeToString(fileContent);
+            out.println("<img src=" + newImage + " />");
         }
     }
 
